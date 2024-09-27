8964200f81ee51301a3a7fdf3bbd5a0966dfdec6
PeterMwitiDev <mwitipet@gmal.com>
2024-09-28 00:13:26 +0300

    Edited some features

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -12,7 +12,8 @@ app.use(cors())
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@riorgroupjobportal.baoxcu8.mongodb.net/?retryWrites=true&w=majority&appName=RiorGroupjobportal`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
